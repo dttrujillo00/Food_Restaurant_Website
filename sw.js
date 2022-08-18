47d149cee497b72fed2e05f55995620dee7db26a
dttrujillo00 <dttrujillo00@gmail.com>
2022-08-18 13:29:21 -0400

    Ahora es una PWA basica

---
+++ sw.js
@@ -0,0 +1,60 @@
+let CACHE_NAME = 'Food_Taste';
+let filesToCache = [
+    '/',
+    '/index.html',
+    '/main.js',
+    '/style.css',
+    '/fonts/Poppins-Regular.ttf',
+    '/images',
+    '/images/',
+    '/images/about.jpg',
+    '/images/delivery.jpg',
+    '/images/home.jpg',
+    '/images/menu1.jpg',
+    '/images/menu2.jpg',
+    '/images/menu2192x192.png',
+    '/images/menu2512x512.png',
+    '/images/menu3.jpg',
+    '/images/order.jpg',
+    '/images/shipping.jpg'
+];
+
+
+
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+        .then( (cache) => cache.addAll(filesToCache))
+    )
+})
+
+self.addEventListener('fetch', (e) => {
+    console.log('[Servicio Worker] Recurso obtenido ' + e.request.url);
+
+    e.respondWith(
+        caches.match(e.request)
+        .then( (r) => {
+            return r || fetch(e.request).then( (response) => {
+                return caches.open(CACHE_NAME).then( (cache) => {
+                    cache.put(e.request, response.clone());
+                    return response
+                });
+            });
+        })
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys()
+        .then( keyList => {
+            return Promise.all(keyList.map( key => {
+                if(key !== CACHE_NAME){
+                    return caches.delete(key);
+                }
+            }))
+        })
+    )
+})
